762f6935bfb8b74ed1f26126b048915886b01fd2
gmarvis <sgmarvis@gmail.com>
2023-09-05 16:20:54 +0100

    completed the the navbar reusable component, ~added basic routes for the landing page andthe dashboard page

---
+++ src/compnents/organisms/navBar/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+// css import
+import "./NavBar.css";
+
+//assets import
+import logo from "../../../assets/tasktrec_logo.png";
+
+const NavBar = ({ children }) => {
+  return (
+    <nav className="navContainer">
+      <div className="navbar">
+        <img src={logo} alt="tasktrec logo" />
+        {children}
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
