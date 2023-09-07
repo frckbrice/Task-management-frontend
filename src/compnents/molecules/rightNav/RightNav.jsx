083e138772172588c0b6adcbd1c5c8b0cf682130
nsamedaisy <nsamedaisy@gmail.com>
2023-09-07 14:02:37 +0100

    responsive navbar commit

--- src/compnents/molecules/rightNav/RightNav.jsx
+++ src/compnents/molecules/rightNav/RightNav.jsx
@@ -1,29 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import "./RightNav.css";
+import "font-awesome/css/font-awesome.min.css";
 
-function RightNav() {
-  return (
-    <div className="rightNav">
-      <div>
-        <ul>
-          <li>
-            <a href="index">Features</a>
-          </li>
-          <li>
-            <a href="index">For Teams</a>
-          </li>
-          <li>
-            <a href="index">Get Help</a>
-          </li>
-        </ul>
-      </div>
+class RightNav extends Component {
+  state = { clicked: false };
 
-      <div className="btn">
-        <button className="reg">Sign Up</button>
-        <button className="log">Log In</button>
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+
+  render() {
+    return (
+      <div className="rightNav">
+        <div id="RightText" className={this.state.clicked ? "#RightText active" : "#RightText"}>
+          <ul>
+            <li>
+              <a href="index">Features</a>
+            </li>
+            <li>
+              <a href="index">For Teams</a>
+            </li>
+            <li>
+              <a href="index">Get Help</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="btn">
+          <button className="reg">Sign Up</button>
+          <button className="log">Log In</button>
+        </div>
+
+        <div id="mobile">
+          <i
+            id="bar"
+            className={this.state.clicked ? "fa fa-times" : "fa fa-bars"}
+            onClick={this.handleClick}
+          ></i>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default RightNav;
