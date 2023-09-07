85e01975141c43e1a6e2f3ecac32069a7984a01d
nsamedaisy <nsamedaisy@gmail.com>
2023-09-07 19:20:17 +0100

    article 2 commit

---
+++ src/compnents/organisms/article2/Article2.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Article2.css";
+
+function Article2() {
+  return (
+    <div className="article2">
+      <div className="art2Text">
+        <h4>Features</h4>
+        <ul>
+          <li>Create and Manage Project</li>
+          <li>Create and Mange Tasks</li>
+          <li>Invite Member(s)</li>
+          <li>Assign Task to Member(s)</li>
+          <li>Track Tasks Progress State</li>
+          <li>Generate Project Report(s)</li>
+        </ul>
+      </div>
+
+      <div>
+        <img className="art2Img" src="" alt="Tracking todos" />
+      </div>
+    </div>
+  );
+}
+
+export default Article2;
--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -4,6 +4,7 @@ import NavBar from "../../compnents/organisms/navBar/NavBar";
 import RightNav from "../../compnents/molecules/rightNav/RightNav";
 import Header from "../../compnents/organisms/header/Header";
 import Article1 from "../../compnents/organisms/article1/Article1";
+import Article2 from "../../compnents/organisms/article2/Article2";
 
 const LandingPage = () => {
   return (
@@ -13,6 +14,7 @@ const LandingPage = () => {
       </NavBar>
       <Header />
       <Article1 />
+      <Article2 />
     </>
   );
 };
