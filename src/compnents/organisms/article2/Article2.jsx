8f988a4d695d68880343200867f7999389fdd3fb
nsamedaisy <nsamedaisy@gmail.com>
2023-09-08 12:03:50 +0100

    desktop commit of article2

--- src/compnents/organisms/article2/Article2.jsx
+++ src/compnents/organisms/article2/Article2.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import "./Article2.css";
+import Image from "../../../assets/Art2Img.png";
 
 function Article2() {
   return (
-    <div className="article2">
-      <div className="art2Text">
-        <h4>Features</h4>
-        <ul>
-          <li>Create and Manage Project</li>
-          <li>Create and Mange Tasks</li>
-          <li>Invite Member(s)</li>
-          <li>Assign Task to Member(s)</li>
-          <li>Track Tasks Progress State</li>
-          <li>Generate Project Report(s)</li>
-        </ul>
-      </div>
+      <div className="article2">
+        <div>
+          <img className="art2Img" src={Image} alt="Tracking todos" />
+        </div>
 
-      <div>
-        <img className="art2Img" src="" alt="Tracking todos" />
+        <div className="art2Text">
+          <h4>Features</h4>
+          <ul>
+            <li>Create and Manage Project</li>
+            <li>Create and Mange Tasks</li>
+            <li>Invite Member(s)</li>
+            <li>Assign Task to Member(s)</li>
+            <li>Track Tasks Progress State</li>
+            <li>Generate Project Report(s)</li>
+          </ul>
+        </div>
       </div>
-    </div>
   );
 }
 
