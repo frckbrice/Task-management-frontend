97ce737c26fa4ba8b0c7b87b7e59dbd8206470a5
nsamedaisy <nsamedaisy@gmail.com>
2023-09-08 15:18:54 +0100

    footer commit

---
+++ src/compnents/organisms/footer/Footer.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import "./Footer.css";
+import {
+  FaLinkedin,
+  FaFacebookF,
+  FaTwitterSquare,
+  FaInstagram,
+  FaSlack,
+  FaYoutube,
+} from "react-icons/fa";
+
+function Footer() {
+  return (
+    <div className="footercontainer">
+      <div className="footerIcons">
+        <h4 className="head4">You can find us at</h4>
+        <icons>
+          <FaFacebookF className="icon" />
+          <FaLinkedin className="icon" />
+          <FaTwitterSquare className="icon" />
+          <FaInstagram className="icon" />
+          <FaSlack className="icon" />
+          <FaYoutube className="icon" />
+        </icons>
+      </div>
+
+      <div className="footer-divs">
+        <div className="div1 div">
+          <h4>Let's Talk</h4>
+          <p className="lettalkPar">Every project starts with a chat GDB leads our client conversations and will be happy to discuss your project. She will also pull in the right people from the team when needed.</p>
+          <h5>Tell us about your project</h5>
+        </div>
+
+        <div className="div2 div">
+          <h4>Company</h4>
+          <p>About</p>
+          <p>Careers</p>
+          <p>Support</p>
+          <p>Pricing</p>
+          <p>FAQ</p>
+        </div>
+
+        <div className="div3 div">
+        <h4>Resources</h4>
+          <p>eBooks</p>
+          <p>Comparison Guide</p>
+          <p>Blog</p>
+          <p>Website Grader</p>
+          <p>Whitepapers</p>
+        </div>
+
+        <div className="div4 div">
+        <h4>Get Help</h4>
+          <p>Help Center</p>
+          <p>Contact Us</p>
+          <p>Privacy Policy</p>
+          <p>Terms</p>
+          <p>Login</p>
+        </div>
+
+      </div>
+
+      <div className="copyrightfooter">
+        <p>Copyright 2020-2023 GDB dev's| <span>It's free for use</span></p>
+      </div>
+
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -6,6 +6,7 @@ import Header from "../../compnents/organisms/header/Header";
 import Article1 from "../../compnents/organisms/article1/Article1";
 import Article2 from "../../compnents/organisms/article2/Article2";
 import Article3 from "../../compnents/organisms/article3/Article3";
+import Footer from "../../compnents/organisms/footer/Footer";
 
 const LandingPage = () => {
   return (
@@ -17,6 +18,7 @@ const LandingPage = () => {
       <Article1 />
       <Article2 />
       <Article3 />
+      <Footer />
     </>
   );
 };
