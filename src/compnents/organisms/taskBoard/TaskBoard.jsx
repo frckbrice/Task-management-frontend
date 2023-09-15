bca1126682ad1421d4be4cdedb1910053f44073f
gmarvis <sgmarvis@gmail.com>
2023-09-15 18:55:25 +0100

    solving drag n drop

--- src/compnents/organisms/taskBoard/TaskBoard.jsx
+++ src/compnents/organisms/taskBoard/TaskBoard.jsx
@@ -103,19 +103,14 @@ const TaskBoard = () => {
     setShowAddTask((prev) => !prev);
   };
 
-
   const handleOpentask = () => {
-
-
     setOpenTask(!openTask);
   };
 
   return (
     <>
       {showAddTask && <OverLay action={togglePopup} />}
-
       {openTask && <OverLay action={handleOpentask} />}
-
       <div className="task-board">
         {showAddTask && (
           <div className="add-task">
@@ -213,9 +208,7 @@ const TaskBoard = () => {
             );
           })}
         </DragDropContext>
-
         {openTask && <TaskOpen onClick={handleOpentask} />}
-
       </div>
     </>
   );
