ec8325773c81d67d959bc609cb57aa92307d4097
AVOM  Brice <maebrie2017@gmail.com>
2024-01-18 19:36:30 +0100

    Update config.js
    update the header

--- src/config.js
+++ src/config.js
@@ -28,7 +28,7 @@ export const conf = {
     "Access-Control-Allow-Headers":
       "Origin, X-Requested-With, Content-Type, Accept",
     "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
-    "access-controll-allow-origin":
-      "https://tms-app-frontend.vercel.app/, http://localhost:3000",
+    "access-control-allow-origin":
+      "https://tms-app-frontend.vercel.app",
   },
 };
